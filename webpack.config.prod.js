8b0fb95117c9bc401f37ea35ee70dd8c0d6cd073
AlwaysAwake <tellmewhy07@korea.ac.kr>
2016-08-05 22:21:44 +0900

    Added initial project settings

---
+++ webpack.config.prod.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+
+  entry: __dirname + '/src/index.js',
+
+  output: {
+    path: __dirname + '/static/dist/',
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css?modules')
+      },
+      {
+        test: /\.jsx*$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new ExtractTextPlugin("app.css")
+  ]
+}
