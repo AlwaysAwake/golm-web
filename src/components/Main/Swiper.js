40d6f7848cde80df89025ebd046ee92d3095e001
lsywind3 <lsywind3@gmail.com>
2016-08-07 05:23:04 +0900

    add swipe img

--- src/components/Main/Swiper.js
+++ src/components/Main/Swiper.js
@@ -10,7 +10,7 @@ const sliderSettings = {
 const Swiper = () => {
   return (
     <Slider {...sliderSettings}>
-      <div><img className="centered" src="http://placehold.it/400x200" alt=""/></div>
+      <div><img className="centered" src="https://s3.ap-northeast-2.amazonaws.com/leefwangbucket/gokathon/images/main_banner_01.png" alt=""/></div>
       <div><img className="centered" src="http://placehold.it/400x200" alt=""/></div>
       <div><img className="centered" src="http://placehold.it/400x200" alt=""/></div>
       <div><img className="centered" src="http://placehold.it/400x200" alt=""/></div>
