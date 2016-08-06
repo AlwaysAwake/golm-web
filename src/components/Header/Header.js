a4961e2950be191dc67e30c6d914497b97bfeb91
AlwaysAwake <tellmewhy07@korea.ac.kr>
2016-08-06 19:31:00 +0900

    Added header link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,40 @@ import React, { PropTypes, Component } from 'react';
 
 
 class Header extends Component {
+  onClickMain(e) {
+    e.preventDefault();
+    this.context.router.push('/');
+  }
+
+  onClickSignin(e) {
+    e.preventDefault();
+    this.context.router.push('/signin');
+  }
+
+  onClickSignup(e) {
+    e.preventDefault();
+    this.context.router.push('/signup');
+  }
+
   render() {
     return (
       <nav className="navbar navbar-dark bg-inverse">
-        <a href="#" className="navbar-brand">GOLM</a>
-        <ul className="nav navbar-nav">
+        <a onClick={(e) => this.onClickMain(e)} href="#" className="navbar-brand">GOLM</a>
+        <ul className="nav navbar-nav pull-sm-right">
+          <li className="nav-item">
+            <a onClick={(e) => this.onClickSignin(e)} href="#" className="nav-link">Sign in</a>
+          </li>
+          <li className="nav-item">
+            <a onClick={(e) => this.onClickSignup(e)} href="#" className="nav-link">Sign up</a>
+          </li>
         </ul>
       </nav>
     );
   }
 }
 
-Header.propTypes = {
-
+Header.contextTypes = {
+  router: PropTypes.object.isRequired,
 };
 
 export default Header;
