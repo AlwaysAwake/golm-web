34b74a9d6c0a7d0277148bbad4345cf21231699b
lsywind3 <lsywind3@gmail.com>
2016-08-07 07:28:20 +0900

    no image found, swipe 이미지 변경

--- src/components/Main/Swiper.js
+++ src/components/Main/Swiper.js
@@ -11,6 +11,7 @@ const Swiper = () => {
   return (
     <Slider {...sliderSettings}>
       <div><img className="centered" src="https://s3.ap-northeast-2.amazonaws.com/leefwangbucket/gokathon/images/main_banner_01.png" alt=""/></div>
+      <div><img className="centered" src="https://s3.ap-northeast-2.amazonaws.com/leefwangbucket/gokathon/images/main_banner_04.png" alt=""/></div>
       <div><img className="centered" src="https://s3.ap-northeast-2.amazonaws.com/leefwangbucket/gokathon/images/main_banner_02.png" alt=""/></div>
       <div><img className="centered" src="https://s3.ap-northeast-2.amazonaws.com/leefwangbucket/gokathon/images/main_banner_03.png" alt=""/></div>
     </Slider>
