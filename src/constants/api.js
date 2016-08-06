b5394649ff336e519be6a0923e8eae68f0dd0ef1
AlwaysAwake <tellmewhy07@korea.ac.kr>
2016-08-07 08:56:17 +0900

    Changed api address

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
 export const apiBaseURL = {
-  production: 'http://localhost:3000',
+  production: 'http://52.79.173.235:3000',
   dev: 'http://localhost:3000',
 }[process.env.NODE_ENV || 'production'];
