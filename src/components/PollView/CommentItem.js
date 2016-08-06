85231b0d12cb427065f8fb0370a9d2f6ab304691
AlwaysAwake <tellmewhy07@korea.ac.kr>
2016-08-06 23:36:58 +0900

    Added styles

--- src/components/PollView/CommentItem.js
+++ src/components/PollView/CommentItem.js
@@ -3,8 +3,16 @@ import React, { PropTypes } from 'react';
 
 const CommentItem = (props) => {
   return (
-    <div className="comment-item">
-
+    <div className="comment-item row">
+      <div className="col-sm-2" style={{ borderRight: '2px solid #000' }}>
+        {props.user.nickname}
+      </div>
+      <div className="col-sm-6">
+        {props.comment}
+      </div>
+      <div className="col-sm-4 text-right">
+        {props.created_at}
+      </div>
     </div>
   );
 };
